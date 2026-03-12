5f5296d0e71a2e732a3e7964af2a324ce64ad005
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-03-12 13:44:12 -0400

    moved projects into data folder, update paths

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 
 
 function Project({ project_data }) {
+    const IMG_PATH = "/data/projects/images/";
     const [project] = useState(project_data);
     
     return (
         <div className="centered-project-box">
-            <img src={`/images/${project.img_name}`} alt={project.img_alt} style={{objectFit:'contain', borderRadius:'3px 3px 0px 0px'}}/>
+            <img src={`${IMG_PATH}${project.img_name}`} alt={project.img_alt} style={{objectFit:'contain', borderRadius:'3px 3px 0px 0px'}}/>
             <div style={{margin:'0', padding:'0'}}>
                 <Link to={project.link}><h4 style={{display:'inline-block'}}>{project.title}</h4></Link>
             </div>
