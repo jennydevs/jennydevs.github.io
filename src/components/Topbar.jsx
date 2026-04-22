6122331371180e8df9776989a075ce1d66a5ca76
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-22 13:56:48 -0400

    topbar is now white

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -8,9 +8,17 @@ function Topbar({header_data}) {
         <div className='topbar'>
             <Header header_data={header_data} />
             <nav className='topbar-nav'>
-                <li className='hori-top-spacer'><Link to={'/'} className='topbar-link'>Home</Link></li>
-                <li className='hori-top-spacer'><Link to={'/projects'} className='topbar-link'>Projects</Link></li>
-                <li className='hori-top-spacer'><Link to={'/devlogs'} className='topbar-link'>Devlogs</Link></li>
+                <li className='topbar-link-holder'>
+                    <Link to={'/'} className='topbar-link'>Home</Link>
+                    <img className='topbar-link-spacer'/>
+                </li>
+                <li className='topbar-link-holder'>
+                    <Link to={'/projects'} className='topbar-link'>Projects</Link>
+                    <img className='topbar-link-spacer'/>
+                </li>
+                <li className='topbar-link-holder'>
+                    <Link to={'/devlogs'} className='topbar-link'>Devlogs</Link>
+                </li>
             </nav>
         </div>
     
