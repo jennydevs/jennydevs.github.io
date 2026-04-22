051dd7ecb291b9f888b6637ce045c18283e057eb
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-22 17:23:50 -0400

    create imglogo component

---
+++ src/components/ImgLogo.jsx
@@ -0,0 +1,18 @@
+
+
+function ImgLogo({logo_name}) {
+    const IMAGE_PATH = "./data/decor/";
+    const IMG_LOGOS = {
+        "bluesky": "bluesky_logo.jpg",
+        "github": "github_logo.jpg",
+        "itch": "itch_logo.jpg",
+        "mastodon": "mastodon_logo.jpg",
+        "youtube": "youtube_logo.jpg"
+    };
+
+    return (
+        <img className='logo' src={`${IMAGE_PATH}${IMG_LOGOS[logo_name]}`} />
+    );
+}
+
+export default ImgLogo;
