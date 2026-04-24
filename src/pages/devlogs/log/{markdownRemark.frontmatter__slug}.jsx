857ac5caf3e156a14ae7299c79c78b3e0a7b3f33
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-23 21:37:27 -0400

    move devlog format stuff from Log over to {markdownRemark.frontmatter__slug}

---
+++ src/pages/devlogs/log/{markdownRemark.frontmatter__slug}.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Topbar from '../../../components/Topbar.jsx';
+import Footer from '../../../components/Footer.jsx';
+
+export default function DevlogTemplate({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+      <div className='container'>
+        <Topbar header_data={'Devlog'} />
+        <div className="content">
+          <img className='img-border' alt="" />
+          <div className="log">
+            <article>
+              <h1>{frontmatter.title}</h1>
+              <p>{frontmatter.date} - (Updated {frontmatter.edit_date})</p>
+              <hr />
+              <div dangerouslySetInnerHTML={{ __html: html }} />
+            </article>
+          </div>
+          <div style={{textAlign:'center', paddingBottom:'20px'}}>
+            <Link to={"/devlogs"}>To devlogs</Link>
+          </div>
+          <img className='img-border' alt="" />
+        </div>
+        <Footer />
+      </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        edit_date(formatString: "MMMM DD, YYYY" )
+        slug
+        title
+        description
+        tags
+      }
+    }
+  }
+`;
