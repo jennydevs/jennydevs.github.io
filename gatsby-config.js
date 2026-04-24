d34f8d02e79f277b821948dc623b14eba3b11929
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-23 21:16:35 -0400

    update config and install pkgs for images, remove trailing slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,44 @@
 module.exports = {
   siteMetadata: {
     title: `jennydevs.github.io`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://jennydevs.github.io`
   },
-  plugins: ["gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-sitemap", 
+    "gatsby-plugin-mdx", 
+    "gatsby-transformer-remark", 
+    "gatsby-plugin-image", 
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-remark-images",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "pages",
+        "path": "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+          }
+        ]
+      }
+    },
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+          }
+        ]
+      }
+    }
+  ],
+  trailingSlash: "ignore"
 };
