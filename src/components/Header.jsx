cb0cf43e340153667746defa0670f0345705f632
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-25 18:22:33 -0400

    add link to header image to return home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 function Header({ header_data }) {
     return (
         <header className='topbar-header'>
-            <img className='header-image'/><h1 className='header-title'>{header_data}</h1>
+            <Link to='/'><img className='header-image'/></Link><h1 className='header-title'>{header_data}</h1>
         </header>
     );
 }
