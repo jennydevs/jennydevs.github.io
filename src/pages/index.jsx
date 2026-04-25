5ca3da7171b87ce267f85b7f0d01792759502da6
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-25 08:16:22 -0400

    add alts to imgs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,16 +9,16 @@ function Home() {
         <div className="container">
             <Topbar header_data={'jennydevs'}/>
             <div className='content'>
-                <img className='img-border'/>
+                <img className='img-border' alt="" />
                 <h1>Hello there!</h1>
                 <p>I mainly work on developing games and custom software tools. Otherwise you'll find me working on things like digital art and printmaking in my spare time... or perhaps updating my website again.</p>
                 <h2>Right now I'm currently working on...</h2>
                 <p>A cute little game that will be revealed this year. You can find updates on it on my <a href="https://bsky.app/profile/jennydevs.bsky.social">bluesky</a>.</p>
                 <hr/>
                 <h1>Recent Projects</h1>
-                <img className='img-border'/>
+                <img className='img-border' alt="" />
                 <ProjectList amt_of_projects={3} year={2025} oldest_first={false}/>
-                <img className='img-border'/>
+                <img className='img-border' alt="" />
             </div>
             <Footer />
         </div>
