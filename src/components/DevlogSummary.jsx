367d940942834ae2ce4a0a27631fbd413eb9315f
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-24 23:52:13 -0400

    update style of devlogsummary

--- src/components/DevlogSummary.jsx
+++ src/components/DevlogSummary.jsx
@@ -6,7 +6,7 @@ function DevlogSummary({ devlog_data }) {
     return (
         <div className="log-summary">
             <Link to={`/devlogs/log${devlog.slug}`}><h2>{devlog.title}</h2></Link>
-            <p><em>Created:</em> {devlog.date} - <em>Updated</em> {devlog.edit_date}</p>
+            <p>Created: {devlog.date} - (Updated: {devlog.edit_date})</p>
             <p>{devlog.description}</p>
         </div>
     );
