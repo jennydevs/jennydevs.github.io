a99f249ca02d92b0ddb5cca2033a7af6ea9901ae
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-04-26 23:45:36 -0400

    fix missing links

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,7 +7,7 @@ function Project({ project_data }) {
         <div className='centered-project-box'>
             <img src={project.image_file} alt={project.alt_text} style={{objectFit:'contain', borderRadius:'3px 3px 0px 0px'}}/>
             <div style={{margin:'0', padding:'0'}}>
-                <a href={project.link}><h4 style={{display:'inline-block'}}>{project.title}</h4></a>
+                <a href={project.project_link}><h4 style={{display:'inline-block'}}>{project.title}</h4></a>
             </div>
             <p style={{margin:'0', padding:'0 5px 5px 5px'}}>{project.description}</p>
         </div>
